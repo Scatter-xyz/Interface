0c4f7cf13eaefbf8c864b53944e16e1c8b9c710f
Phoenix <phoenix@Phoenixs-MacBook-Air.local>
2022-08-14 08:48:08 +0530

    build successfull

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import '../styles/globals.css'
 
-const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/cpp-phoenix/scatter/",
-  cache: new InMemoryCache()
-});
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} /> 
-    </ApolloProvider>
+    <Component {...pageProps} /> 
   );
 }
 
